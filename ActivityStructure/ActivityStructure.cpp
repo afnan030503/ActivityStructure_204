7ef12195edfe09ce48c4776f081e3bd18495111a
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-04-03 14:43:04 +0700

    deskrpsikan dari entitas

--- ActivityStructure/ActivityStructure.cpp
+++ ActivityStructure/ActivityStructure.cpp
@@ -7,7 +7,7 @@ struct Mahasiswa {
 	string nama;
 	string alamat;
 	string umur;
-}; 
+};
 
 int main()
 {
@@ -17,7 +17,24 @@ int main()
 	mhs1.nama = "Afnan";
 	mhs1.alamat = "camplong";
 	mhs1.umur = 19;
-} 
 
-cout << "Masukkan nim: ";
-cin >>  mhs
+
+	cout << "Masukkan nim: ";
+	cin >> mhs2.nim;
+	cout << "Masukkan nama: ";
+	cin >> mhs2.nama;
+	cout << "Masukkan alamat: ";
+	cin >> mhs2.alamat;
+	cout << "Masukkan umur: ";
+	cin >> mhs2.umur;
+
+	cout << "\nnim : " << mhs1.nim;
+	cout << "\nnama : " << mhs1.nama;
+	cout << "\nalamat : " << mhs1.alamat;
+	cout << "\numur : " << mhs1.umur;
+
+	cout << "\nnim : " << mhs2.nim;
+	cout << "\nnama : " << mhs2.nama;
+	cout << "\nalamat : " << mhs2.alamat;
+	cout << "\numur : " << mhs2.umur;
+}
