a4d686e8fa47e4ca729f48fc2b83b7f546304bf3
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-04-03 15:10:38 +0700

    entitas

--- ActivityStructure2/ActivityStructure2.cpp
+++ ActivityStructure2/ActivityStructure2.cpp
@@ -7,4 +7,29 @@ struct AlamatDetail
 	string desa;
 	string kota;
 };
-struct Mhasiswa
+struct Mahasiswa
+{
+	string nim;
+	string nama;
+	AlamatDetail alamat;
+	int umur;
+};
+int main() {
+	Mahasiswa mhs;
+	cout << "Masukkan nim: ";
+	cin >> mhs.nim;
+	cout << "Masukkan nama: ";
+	cin >> mhs.nama;
+	cout << "Alamat: " << endl;
+	cout << "\tnamadesa: ";
+	cin >> mhs.alamat.desa;
+	cout << "\tnama kota: ";
+	cin >> mhs.alamat.kota;
+	cout << "Masukkan umur: ";
+	cin >> mhs.umur;
+
+	cout << "\nnim : " << mhs.nim;
+	cout << "\nama : " << mhs.nama;
+	cout << "\ndesa : " << mhs.alamat.desa;
+	cout << "\nkota : " << mhs.alamat.kota;
+}
---
+++ activityStructure3/activityStructure3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+struct AlamatDetail
+{
+	string desa;
+	string kota;
+};
+struct Mahasiswa
+{
+	string nim;
+	string nama;
+	AlamatDetail alamat;
+	int umur;
+};
+
+int main() {
+	Mahasiswa mhs[3];
+
+	for (int i = 0;)
