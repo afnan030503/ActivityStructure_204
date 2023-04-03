66bf9d9cb3b75b7e1f5028802a11431542535817
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-04-03 14:35:25 +0700

    menambah

--- ActivityStructure/ActivityStructure.cpp
+++ ActivityStructure/ActivityStructure.cpp
@@ -8,3 +8,16 @@ struct Mahasiswa {
 	string alamat;
 	string umur;
 }; 
+
+int main()
+{
+	Mahasiswa mhs1, mhs2;
+
+	mhs1.nim = "20220140204";
+	mhs1.nama = "Afnan";
+	mhs1.alamat = "camplong";
+	mhs1.umur = 19;
+} 
+
+cout << "Masukkan nim: ";
+cin >>  mhs
